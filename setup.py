2a89193f835fb4fc03f2786db7151d8843177833
Wolfgang Schwack <food-science@ernaehrung.uni-giessen.de>
2021-01-21 13:30:14 +0100

    corrections

--- setup.py
+++ setup.py
@@ -46,31 +46,4 @@ def cancelprint():
   p.cancelprint()
   
 
-
-# # LED strip configuration:
-# LED_COUNT      = 16      # Number of LED pixels.
-# LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
-# LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-# LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-# LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-# LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-# LED_CHANNEL    = 0
-# LED_STRIP      = ws.SK6812_STRIP_RGBW
-# #LED_STRIP      = ws.SK6812W_STRIP
-# strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-# # Intialize the library (must be called once before other functions).
-# strip.begin()
-# 
-# 
-# # Define functions which animate LEDs in various ways.
-# def colorWipe(number=0, wait_ms=50):
-# 	"""Wipe color across display a pixel at a time."""
-# 	color = Color(number,number,number,number)
-# 	for i in range(strip.numPixels()):
-# 		strip.setPixelColor(i, color)
-# 		strip.show()
-# 		time.sleep(wait_ms/1000.0)
-
-
-
 atexit.register(close_connections)
